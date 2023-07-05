3ae29961e49e200c49ffba329354a3386414108a
Mahadi Hassan <mm.hassan889@gmail.com>
2023-07-05 16:40:58 +0600

    feat: add job-posts pages

---
+++ src/pages/job-posts/show.tsx
@@ -0,0 +1,6 @@
+import { IResourceComponentsProps } from "@refinedev/core";
+import { MuiShowInferencer } from "@refinedev/inferencer/mui";
+
+export const JobPostShow: React.FC<IResourceComponentsProps> = () => {
+  return <MuiShowInferencer />;
+};
